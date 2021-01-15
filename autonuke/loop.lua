f4a7606f366ed3124d1efb5445ee59dd91ea20d0
Sphyix <masifede2@gmail.com>
2021-01-15 23:50:23 +0100

    abc

--- autonuke/loop.lua
+++ autonuke/loop.lua
@@ -1,14 +1,29 @@
-while true do
-	os.sleep(0.1)
+	--startup
+	
 	local component = require("component")
 	local sides = require("sides")
 
 	rsOutput = component.proxy("2009c856-ba4d-4449-afb4-37ceae46619d")
 	rsRefill = component.proxy("81161fbe-f767-4ae0-acc2-ca869b393692")
 
+
+	rsOutput.setOutput(sides.east, 0)
+	startCoolant = rsOutput.getInput(sides.west)
+	startCell = rsOutput.getInput(sides.south)
+	if (coolant > 0 and cell > 0) then			-- *Both coolant and cell, something went wrong*
+		print("Something went wrong")
+		rsOutput.setOutput(sides.east, 14) 		--stop output
+		rsOutput.setOutput(sides.bottom, 0)		--stop reactor TODO turn on some lights?
+	else
+		rsOutput.setOutput(sides.bottom, 14)	--start reactor
+	end
+
+
+while true do
+	os.sleep(0.1)
+
 	local coolant = rsOutput.getInput(sides.west)
 	local cell = rsOutput.getInput(sides.south)
-
 	--rsOutput
 		--east: export from reactor
 		--bottom: turn on and off reactor
@@ -24,21 +39,26 @@ while true do
 		print("Something went wrong")
 		rsOutput.setOutput(sides.east, 14) 		--stop output
 		rsOutput.setOutput(sides.bottom, 0)		--stop reactor TODO turn on some lights?
-	elseif (coolant > 0 and cell = 0) then		-- *Coolant, stop reactor, process and refill, start reactor*
+	elseif (coolant > 0 and cell == 0) then		-- *Coolant, stop reactor, process and refill, start reactor*
 		print("Changing coolant")
-		rsOutput.setOutput(sides.bottom, 0)		--stop reactor
 		rsOutput.setOutput(sides.east, 14) 		--stop output, process coolant
+		rsOutput.setOutput(sides.bottom, 0)		--stop reactor
 		rsRefill.setOutput(sides.north, 14) 	--refill coolant
 		os.sleep(1)
 		rsRefill.setOutput(sides.north, 0) 		--stop refill
 		rsOutput.setOutput(sides.bottom, 14) 	--start reactor
-	elseif(coolant = 0 and cell > 0) then		-- *Quad, stop reactor,  refill, start reactor*
+		rsOutput.setOutput(sides.east, 0)		--start output
+		print("Coolant changed")
+	elseif(coolant == 0 and cell > 0) then		-- *Quad, stop reactor,  refill, start reactor*
 		print("Changing quad")
+		rsOutput.setOutput(sides.east, 14) 		--stop output, process coolant
 		rsOutput.setOutput(sides.bottom, 0)		--stop reactor
 		rsRefill.setOutput(sides.west, 14) 		--refill quad
 		os.sleep(1)
 		rsRefill.setOutput(sides.west, 0) 		--stop refill
 		rsOutput.setOutput(sides.bottom, 14) 	--start reactor
+		rsOutput.setOutput(sides.east, 0)		--start output
+		print("Qaud changed")
 	end
 end
 
