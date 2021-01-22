8f4466170ed1fd6e70f46f36143ddb9bec83191b
Sphyix <masifede2@gmail.com>
2021-01-22 17:52:27 +0100

    rework

--- autonuke/loop.lua
+++ autonuke/loop.lua
@@ -12,8 +12,7 @@ Colors:
 	Black: output to start extracting depleted
 	Yellow: output to start inserting coolant
 	Grey: output to start inserting rods
-	Green: input if coolant is extracted
-	Purple: input if depleted is extracted
+	Green: input for average EU
 --]]
 local function newReactor(proxyID, rsSide)
 
@@ -22,8 +21,7 @@ local function newReactor(proxyID, rsSide)
 	redstoneSide = rsSide;
 	tempReading = 0; --number, Blue input
 	batteryStatus = 0; --number, Light grey input
-	coolantExtracted = 0;--number, Green input
-	depletedExtracted = 0; --number, Purple input
+	avgEU = 0;--number, Green input
 	--reactorStatus --boolean, White output
 	--coolantExport	--boolean, Red output
 	--depletedExport --boolean, Black output
@@ -86,59 +84,53 @@ local function checkForTemperature(reactor)
 	end
 end
 
-local function checkForCoolant(reactor)
+local function startChangeCoolant(reactor)
+	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.red, 255)
+	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.yellow, 255)
+end
+
+local function stopChangeCoolant(reactor)
 	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.red, 255)
 	os.sleep(1)
-	updateValues(reactor)
-	if(reactor.coolantExtracted>0) then
-		print("Started changing coolant")
-		reactor.comp.setBundledOutput(reactor.redstoneSide, colors.yellow, 255)
-		while(reactor.coolantExtracted>0) do
-			print("Changing coolant...")
-			os.sleep(1)
-			updateValues(reactor)
-		end
-		reactor.comp.setBundledOutput(reactor.redstoneSide, colors.yellow, 0)
-		print("Finished changing coolant")
-	end
-	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.red, 0)
+	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.yellow, 255)
+end
+
+local function changeDepleted(reactor)
+	print("Changing Rods")
+	turnOffReactor(reactor)
+	stopChangeCoolant(reactor)
+	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.yellow, 0)
+	os.sleep(1)
+	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.black, 255)
+	os.sleep(5) 
+	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.black, 0)
+	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.grey, 255)
+	os.sleep(5)
+	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.grey, 0)
+	turnOnReactor(reactor)
+	print("Rods Changed")
+	os.sleep(1)
+	startChangeCoolant(reactor)
 end
 
 local function checkForDepleted(reactor)
-	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.black, 255)
-	os.sleep(1)
 	updateValues(reactor)
-	if(reactor.depletedExtracted>0) then
-		print("Started changing rods")
-		turnOffReactor(reactor)
-	
-		local count = 1
-		while(reactor.depletedExtracted>0) do
-			print("Changing n " .. count)
-			reactor.comp.setBundledOutput(reactor.redstoneSide, colors.gray, 255)
-			os.sleep(1)
-			reactor.comp.setBundledOutput(reactor.redstoneSide, colors.gray, 0)
-			os.sleep(1)
-			updateValues(reactor)
-			count = count + 1
-		end
-		print("Finished changing rods")
-		turnOnReactor(reactor)
+	if(reactor.batteryStatus<20 and reactor.comp.getBundledOutput(reactor.redstoneSide, colors.white) > 0 and reactor.avgEU == 0) then
+		print("Rods depleted, getting ready to change them")
 	end
-	reactor.comp.setBundledOutput(reactor.redstoneSide, colors.black, 0)
 end
 
 local function checkForBatteryStatus(reactor)
 	local latch = false
 	repeat
 		updateValues(reactor)
-		if(reactor.batteryStatus>14 and not latch) then
+		if(reactor.batteryStatus>215 and not latch) then --max 221
 			print("Battery full, stopping reactor" .. reactor.batteryStatus)
 			turnOffReactor(reactor)
 			latch = true
 		end
 
-		if(reactor.batteryStatus<2 and latch ) then
+		if(reactor.batteryStatus<20 and latch ) then
 			print("Battery depleted, restarting reactor" .. reactor.batteryStatus)
 			turnOnReactor(reactor)
 			latch = false
@@ -152,10 +144,11 @@ end
 local function initialize(reactor)
 	updateValues(reactor)
 	checkForTemperature(reactor)
-	checkForCoolant(reactor)
-	checkForDepleted(reactor)
 	checkForBatteryStatus(reactor)
+	startChangeCoolant(reactor)
 	turnOnReactor(reactor)
+	os.sleep(2)
+	checkForDepleted(reactor)
 end
 
 local rs1Code = "2009c856-ba4d-4449-afb4-37ceae46619d"
@@ -167,11 +160,9 @@ initialize(reactor)
 while(true) do
 	updateValues(reactor)
 	checkForTemperature(reactor)
-	checkForCoolant(reactor)
-	checkForDepleted(reactor)
 	checkForBatteryStatus(reactor)
-
-	os.sleep(1)
+	os.sleep(2)
+	checkForDepleted(reactor)
 end
 
 
